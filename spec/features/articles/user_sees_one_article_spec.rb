f86ae81ffd69b9455782640a292fa51a87a67d15
Nick Edwin <nickedwin85@gmail.com>
2020-07-19 13:54:42 -0600

    Comment display test

--- spec/features/articles/user_sees_one_article_spec.rb
+++ spec/features/articles/user_sees_one_article_spec.rb
@@ -4,6 +4,8 @@ describe "user sees one article" do
   describe "they link from the article index" do
     it "displays information for one article" do
       article = Article.create!(title: "New Title", body: "New Body")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
 
